688057424ebbe0e5c3e6996edd8a772d2371b014
Connor Ivy <connorivy15@utexas.edu>
2025-03-26 10:19:43 -0500

    mini surround and more vs maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,66 +2,208 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+local function ToggleWordWrap()
+  if vim.wo.wrap then
+    vim.wo.wrap = false
+    print("Word wrap disabled")
+  else
+    vim.wo.wrap = true
+    print("Word wrap enabled")
+  end
+end
+
+local function copyToClipBoard()
+  vim.cmd("set clipboard+=unnamedplus")
+  vim.cmd("norm! y")
+  vim.cmd("set clipboard-=unnamedplus")
+  print("copied!")
+end
+
+local function callVSCodeFunction(vsCodeCommand)
+  vim.cmd(vsCodeCommand)
+end
+
+local function vscodeMappings()
+  map("n", "<C-/>", function()
+    callVSCodeFunction("call VSCodeCall('workbench.action.terminal.focus')")
+  end, { noremap = true, silent = true, desc = "toggle terminal" })
+
+  map("t", "<C-l>", function()
+    print("next term")
+    callVSCodeFunction("call VSCodeCall('workbench.action.terminal.focusNextPane')")
+  end, { noremap = true, silent = true, desc = "cycle terminal focus" })
+
+  map("t", "<C-h>", function()
+    print("prev term")
+    callVSCodeFunction("call VSCodeCall('workbench.action.terminal.focusPreviousPane')")
+  end, { noremap = true, silent = true, desc = "cycle terminal focus" })
+
+  map("n", "<leader>cs", function()
+    print("go to symbols in editor")
+    callVSCodeFunction("call VSCodeCall('workbench.action.gotoSymbol')")
+  end, { noremap = true, silent = true, desc = "go to symbols in editor" })
+
+  map("n", "<S-l>", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.nextEditor')")
+  end, { noremap = true, desc = "switch between editor to next" })
+
+  map("n", "<S-h>", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.previousEditor')")
+  end, { noremap = true, desc = "switch between editor to previous" })
+
+  map("n", "gr", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.referenceSearch.trigger')")
+  end, { noremap = true, desc = "peek references inside vs code" })
+
+  map("n", "<leader>sd", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.problems.focus')")
+  end, { noremap = true, desc = "open problems and errors infos" })
+
+  map("n", "<leader>e", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.files.action.focusFilesExplorer')")
+  end, { noremap = true, desc = "focus to file explorer" })
+
+  map("n", "<leader>fe", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.files.action.focusFilesExplorer')")
+  end, { noremap = true, desc = "focus to file explorer" })
+
+  map("n", "<leader>ff", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.quickOpen')")
+  end, { noremap = true, desc = "open files" })
+
+  map("n", "<leader>gg", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.view.scm')")
+  end, { noremap = true, desc = "open git source control" })
+
+  map("n", "<leader>sml", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.list')")
+  end, { noremap = true, desc = "open bookmarks list for current files" })
+
+  map("n", "<leader>smL", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.listFromAllFiles')")
+  end, { noremap = true, desc = "open bookmarks list for all files" })
+
+  map("n", "<leader>smm", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.toggle')")
+  end, { noremap = true, desc = "toggle bookmarks" })
+
+  map("n", "<leader>smd", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.clear')")
+  end, { noremap = true, desc = "clear bookmarks from current file" })
+
+  map("n", "<leader>smr", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.clearFromAllFiles')")
+  end, { noremap = true, desc = "clear bookmarks from all file" })
+
+  map("n", "<leader>cr", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.rename')")
+  end, { noremap = true, desc = "rename symbol" })
+
+  map("n", "<leader>ca", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.quickFix')")
+  end, { noremap = true, desc = "open quick fix in vs code" })
+
+  map("n", "<leader>cA", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.sourceAction')")
+  end, { noremap = true, desc = "open source Action in vs code" })
+
+  map("n", "<leader>cp", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.panel.markers.view.focus')")
+  end, { noremap = true, desc = "open problems diagnostics" })
+
+  map("n", "<leader>cd", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.marker.next')")
+  end, { noremap = true, desc = "open problems diagnostics" })
+
+  map({ "v" }, "<C-c>", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.clipboardCopyAction')")
+    print("📎added to clipboard!")
+  end, { noremap = true, desc = "copy text/add text to clipboard" })
+
+  map({ "n" }, "u", function()
+    callVSCodeFunction("call VSCodeNotify('undo')")
+  end, { noremap = true, desc = "undo changes" })
+
+  map({ "n" }, "<C-r>", function()
+    callVSCodeFunction("call VSCodeNotify('redo')")
+  end, { noremap = true, desc = "redo changes" })
+end
 
 if vim.g.vscode then
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
+  local keymap = vim.keymap.set
+  local opts = { noremap = true, silent = true }
 
--- remap leader key
-keymap("n", "<Space>", "", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+  vscodeMappings()
 
--- yank to system clipboard
-keymap({"n", "v"}, "<leader>y", '"+y', opts)
+  -- remap leader key
+  keymap("n", "<Space>", "", opts)
+  vim.g.mapleader = " "
+  vim.g.maplocalleader = " "
 
--- paste from system clipboard
-keymap({"n", "v"}, "<leader>p", '"+p', opts)
+  -- yank to system clipboard
+  keymap({ "n", "v" }, "<leader>y", '"+y', opts)
 
--- better indent handling
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+  -- paste from system clipboard
+  keymap({ "n", "v" }, "<leader>p", '"+p', opts)
 
--- move text up and down
-keymap("v", "J", ":m .+1<CR>==", opts)
-keymap("v", "K", ":m .-2<CR>==", opts)
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+  -- better indent handling
+  keymap("v", "<", "<gv", opts)
+  keymap("v", ">", ">gv", opts)
 
--- paste preserves primal yanked piece
-keymap("v", "p", '"_dP', opts)
+  -- move text up and down
+  keymap("v", "J", ":m .+1<CR>==", opts)
+  keymap("v", "K", ":m .-2<CR>==", opts)
+  keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+  keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
--- removes highlighting after escaping vim search
-keymap("n", "<Esc>", "<Esc>:noh<CR>", opts)
+  -- paste preserves primal yanked piece
+  keymap("v", "p", '"_dP', opts)
 
--- general keymaps
-keymap({"n", "v"}, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
-keymap({"n", "v"}, "<leader>b", "<cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>")
-keymap({"n", "v"}, "<leader>d", "<cmd>lua require('vscode').action('editor.action.showHover')<CR>")
-keymap({"n", "v"}, "<leader>a", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
-keymap({"n", "v"}, "<leader>sp", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
-keymap({"n", "v"}, "<leader>cn", "<cmd>lua require('vscode').action('notifications.clearAll')<CR>")
-keymap({"n", "v"}, "<leader>ff", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
-keymap({"n", "v"}, "<leader>cp", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
-keymap({"n", "v"}, "<leader>pr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
-keymap({"n", "v"}, "<leader>fd", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
+  -- removes highlighting after escaping vim search
+  keymap("n", "<Esc>", "<Esc>:noh<CR>", opts)
 
--- harpoon keymaps
-keymap({"n", "v"}, "<leader>ha", "<cmd>lua require('vscode').action('vscode-harpoon.addEditor')<CR>")
-keymap({"n", "v"}, "<leader>ho", "<cmd>lua require('vscode').action('vscode-harpoon.editorQuickPick')<CR>")
-keymap({"n", "v"}, "<leader>he", "<cmd>lua require('vscode').action('vscode-harpoon.editEditors')<CR>")
-keymap({"n", "v"}, "<leader>h1", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor1')<CR>")
-keymap({"n", "v"}, "<leader>h2", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor2')<CR>")
-keymap({"n", "v"}, "<leader>h3", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor3')<CR>")
-keymap({"n", "v"}, "<leader>h4", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor4')<CR>")
-keymap({"n", "v"}, "<leader>h5", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor5')<CR>")
-keymap({"n", "v"}, "<leader>h6", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor6')<CR>")
-keymap({"n", "v"}, "<leader>h7", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor7')<CR>")
-keymap({"n", "v"}, "<leader>h8", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor8')<CR>")
-keymap({"n", "v"}, "<leader>h9", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor9')<CR>")
+  -- general keymaps
+  keymap({ "n", "v" }, "<leader>t", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
+  keymap({ "n", "v" }, "<leader>b", "<cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>")
+  keymap({ "n", "v" }, "<leader>d", "<cmd>lua require('vscode').action('editor.action.showHover')<CR>")
+  -- keymap({"n", "v"}, "<leader>a", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")
+  keymap({ "n", "v" }, "<leader>sp", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
+  keymap({ "n", "v" }, "<leader>cn", "<cmd>lua require('vscode').action('notifications.clearAll')<CR>")
+  keymap({ "n", "v" }, "<leader>ff", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
+  keymap({ "n", "v" }, "<leader>cp", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
+  keymap({ "n", "v" }, "<leader>pr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
+  keymap({ "n", "v" }, "<leader>fd", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
 
--- project manager keymaps
-keymap({"n", "v"}, "<leader>pa", "<cmd>lua require('vscode').action('projectManager.saveProject')<CR>")
-keymap({"n", "v"}, "<leader>po", "<cmd>lua require('vscode').action('projectManager.listProjectsNewWindow')<CR>")
-keymap({"n", "v"}, "<leader>pe", "<cmd>lua require('vscode').action('projectManager.editProjects')<CR>")
+  -- harpoon keymaps
+  keymap({ "n", "v" }, "<leader>ha", "<cmd>lua require('vscode').action('vscode-harpoon.addEditor')<CR>")
+  keymap({ "n", "v" }, "<leader>ho", "<cmd>lua require('vscode').action('vscode-harpoon.editorQuickPick')<CR>")
+  keymap({ "n", "v" }, "<leader>he", "<cmd>lua require('vscode').action('vscode-harpoon.editEditors')<CR>")
+  keymap({ "n", "v" }, "<leader>h1", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor1')<CR>")
+  keymap({ "n", "v" }, "<leader>h2", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor2')<CR>")
+  keymap({ "n", "v" }, "<leader>h3", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor3')<CR>")
+  keymap({ "n", "v" }, "<leader>h4", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor4')<CR>")
+  keymap({ "n", "v" }, "<leader>h5", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor5')<CR>")
+  keymap({ "n", "v" }, "<leader>h6", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor6')<CR>")
+  keymap({ "n", "v" }, "<leader>h7", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor7')<CR>")
+  keymap({ "n", "v" }, "<leader>h8", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor8')<CR>")
+  keymap({ "n", "v" }, "<leader>h9", "<cmd>lua require('vscode').action('vscode-harpoon.gotoEditor9')<CR>")
+
+  -- project manager keymaps
+  keymap({ "n", "v" }, "<leader>pa", "<cmd>lua require('vscode').action('projectManager.saveProject')<CR>")
+  keymap({ "n", "v" }, "<leader>po", "<cmd>lua require('vscode').action('projectManager.listProjectsNewWindow')<CR>")
+  keymap({ "n", "v" }, "<leader>pe", "<cmd>lua require('vscode').action('projectManager.editProjects')<CR>")
 end
